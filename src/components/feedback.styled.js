470d9037287ac7acd4cae859353034ba358ec2d9
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-09-20 13:38:22 +0300

    шаг 1

---
+++ src/components/feedback.styled.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const BoxFeedback = styled.div``;
+export const Header = styled.h2``;
+export const BoxButton = styled.div``;
+export const Button = styled.button`
+:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+export const HeaderValue = styled.h2``;
+export const BoxValue = styled.div``;
+export const TextValue = styled.p`
+display: block;
+:not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+export const Value = styled.span``;
